856e9b1d7057ddebda838a2e1cbf64c70e8cdd66
Ella Fox <ella@fox.gal>
2026-09-09 11:27:46 +0200

    Restore HSF, RANSAC, AHSF and blink from MIT-licensed upstream in 5b48d5d7

--- EyeTrackApp/blink.py
+++ EyeTrackApp/blink.py
@@ -1,36 +1,6 @@
-"""
-------------------------------------------------------------------------------------------------------
-
-                                               ,@@@@@@
-                                            @@@@@@@@@@@            @@@
-                                          @@@@@@@@@@@@      @@@@@@@@@@@
-                                        @@@@@@@@@@@@@   @@@@@@@@@@@@@@
-                                      @@@@@@@/         ,@@@@@@@@@@@@@
-                                         /@@@@@@@@@@@@@@@  @@@@@@@@
-                                    @@@@@@@@@@@@@@@@@@@@@@@@ @@@@@
-                                @@@@@@@@                @@@@@
-                              ,@@@                        @@@@&
-                                             @@@@@@.       @@@@
-                                   @@@     @@@@@@@@@/      @@@@@
-                                   ,@@@.     @@@@@@((@     @@@@(
-                                   //@@@        ,,  @@@@  @@@@@
-                                   @@@(                @@@@@@@
-                                   @@@  @          @@@@@@@@#
-                                       @@@@@@@@@@@@@@@@@
-                                      @@@@@@@@@@@@@(
-
-Binary Intensity Based Blink by: Summer, Prohurtz
-Algorithm App Implementations and tweaks By: Prohurtz
-
-Copyright (c) 2023 EyeTrackVR <3
-LICENSE: Summer Software Distribution License 1.0
-------------------------------------------------------------------------------------------------------
-"""
-
 import numpy as np
 
-
-def BLINK(self):
+def BLINK(self): 
 
     if self.blink_clear == True:
         self.max_ints = []
@@ -40,18 +10,14 @@ def BLINK(self):
     intensity = np.sum(self.current_image_gray_clean)
 
     if self.calibration_frame_counter == 300:
-        self.filterlist = []  # clear filter
+        self.filterlist = [] #clear filter
     if len(self.filterlist) < 300:
         self.filterlist.append(intensity)
     else:
         self.filterlist.pop(0)
         self.filterlist.append(intensity)
-    if (
-        intensity >= np.percentile(self.filterlist, 99)
-        or intensity <= np.percentile(self.filterlist, 1)
-        and len(self.max_ints) >= 1
-    ):  # filter abnormally high values
-        try:  # I don't want this here but I cant get python to stop crying when it's not
+    if intensity >= np.percentile(self.filterlist, 99) or intensity <= np.percentile(self.filterlist, 1) and len(self.max_ints) >= 1:  # filter abnormally high values
+        try: # I don't want this here but I cant get python to stop crying when it's not
             intensity = min(self.max_ints)
         except:
             pass
@@ -59,7 +25,7 @@ def BLINK(self):
     self.frames = self.frames + 1
     if intensity > self.max_int:
         self.max_int = intensity
-        if self.frames > 300:  # TODO: test this number more (make it a setting??)
+        if self.frames > 300: #TODO: test this number more (make it a setting??)
             self.max_ints.append(self.max_int)
     if intensity < self.min_int:
         self.min_int = intensity
@@ -68,11 +34,9 @@ def BLINK(self):
         if intensity > min(self.max_ints):
             blinkvalue = 0.0
         else:
-            blinkvalue = 0.8
+            blinkvalue = 0.7
     try:
         return blinkvalue
     except:
-        return 0.8
-
-
-# print(self.blinkvalue, self.max_int, self.min_int, self.frames, intensity)
+        return 0.7
+   # print(self.blinkvalue, self.max_int, self.min_int, self.frames, intensity)
--- EyeTrackApp/ransac.py
+++ EyeTrackApp/ransac.py
@@ -23,12 +23,11 @@ RANSAC 3D By: Summer#2406 (Main Algorithm Engineer), Pupil Labs (pye3d), PallasN
 Algorithm App Implementations By: Prohurtz, qdot (Initial App Creator)
 
 Copyright (c) 2023 EyeTrackVR <3
-LICENSE: Summer Software Distribution License 1.0
 ------------------------------------------------------------------------------------------------------
 """
 import cv2
 import numpy as np
-from eye import EyeId
+from enum import IntEnum
 from utils.img_utils import safe_crop
 from utils.misc_utils import clamp
 import os
@@ -44,6 +43,13 @@ except AttributeError:
 else:
     process.nice(psutil.BELOW_NORMAL_PRIORITY_CLASS)  # Windows
     process.nice()
+
+
+class EyeId(IntEnum):
+    RIGHT = 0
+    LEFT = 1
+    BOTH = 2
+    SETTINGS = 3
 
 
 def ellipse_model(data, y, f):
@@ -63,7 +69,7 @@ def ellipse_model(data, y, f):
 def fit_rotated_ellipse_ransac(
     data: np.ndarray,
     rng: np.random.Generator,
-    iter=45,
+    iter=100,
     sample_num=10,
     offset=80,  # 80.0, 10, 80
 ):  # before changing these values, please read up on the ransac algorithm
@@ -111,9 +117,13 @@ def fit_rotated_ellipse_ransac(
 
     # These two lines are one of the bottlenecks
     datamod_rng_5x5 = np.matmul(datamod_rng_swap_trans, datamod_rng_swap)
-    datamod_rng_p5smp = np.matmul(np.linalg.inv(datamod_rng_5x5), datamod_rng_swap_trans)
+    datamod_rng_p5smp = np.matmul(
+        np.linalg.inv(datamod_rng_5x5), datamod_rng_swap_trans
+    )
 
-    datamod_rng_p = np.matmul(datamod_rng_p5smp, datamod_rng6[:, :, np.newaxis]).reshape((-1, 5))
+    datamod_rng_p = np.matmul(
+        datamod_rng_p5smp, datamod_rng6[:, :, np.newaxis]
+    ).reshape((-1, 5))
 
     # I don't think it looks beautiful.
     ellipse_y_arr = np.asarray(
@@ -127,7 +137,9 @@ def fit_rotated_ellipse_ransac(
         dtype=ret_dtype,
     )
 
-    ellipse_data_arr = ellipse_model(datamod_slim, ellipse_y_arr, np.asarray(datamod_rng_p[:, 4])).transpose((1, 0))
+    ellipse_data_arr = ellipse_model(
+        datamod_slim, ellipse_y_arr, np.asarray(datamod_rng_p[:, 4])
+    ).transpose((1, 0))
     ellipse_data_abs = np.abs(ellipse_data_arr)
     ellipse_data_index = np.argmax(np.sum(ellipse_data_abs < offset, axis=1), axis=0)
     effective_data_arr = ellipse_data_arr[ellipse_data_index]
@@ -168,7 +180,7 @@ def fit_rotated_ellipse(data, P):
     w, h = wh[0], wh[1]
 
     error_sum = np.sum(data)
-   # print("fitting error = %.3f" % (error_sum))
+    # print("fitting error = %.3f" % (error_sum))
 
     return (cx, cy, w, h, theta)
 
@@ -206,7 +218,7 @@ cct = 300
 def RANSAC3D(self, hsrac_en):
     f = False
     ranf = False
-    blink = 0.8
+    blink = 0.7
     angle = 0
 
     if hsrac_en:
@@ -260,7 +272,7 @@ def RANSAC3D(self, hsrac_en):
         self.failed = self.failed + 1  # we have failed, move onto next algo
         return 0, 0, 0, frame, blink, 0, 0
     else:
-        frame_gray = cv2.GaussianBlur(frame, (9, 9), 10)
+        frame_gray = cv2.GaussianBlur(frame, (5, 5), 0)
 
     # this will need to be adjusted everytime hardware is changed (brightness of IR, Camera postion, etc)m
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(frame_gray)
@@ -289,7 +301,6 @@ def RANSAC3D(self, hsrac_en):
 
     contours, _ = cv2.findContours(th_frame, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
     hull = []
-    # print(contours)
     # This way is faster than contours[i]
     # But maybe this one is faster. hull = [cv2.convexHull(cnt, False) for cnt in contours]
     for cnt in contours:
@@ -315,7 +326,7 @@ def RANSAC3D(self, hsrac_en):
         # cv2.drawContours(self.current_image_gray, contours, -1, (255, 0, 0), 1)
     # cv2.circle(self.current_image_gray, (cx, cy), 2, (0, 0, 255), -1)
     # cx1, cy1, w1, h1, theta1 = fit_rotated_ellipse(maxcnt.reshape(-1, 2))
-    # cv2.ellipse(self.current_image_gray, (cx, cy), (w, h), theta * 180.0 / np.pi, 0.0, 360.0, (50, 250, 200), 1, )
+        #cv2.ellipse(self.current_image_gray, (cx, cy), (w, h), theta * 180.0 / np.pi, 0.0, 360.0, (50, 250, 200), 1, )
 
     # img = newImage2[y1:y2, x1:x2]
     except:
@@ -323,7 +334,9 @@ def RANSAC3D(self, hsrac_en):
         pass
 
     self.current_image_gray = frame
-    cv2.circle(self.current_image_gray, min_loc, 2, (0, 0, 255), -1)  # the point of the darkest area in the image
+    cv2.circle(
+        self.current_image_gray, min_loc, 2, (0, 0, 255), -1
+    )  # the point of the darkest area in the image
 
     # However eyes are annoyingly three dimensional, so we need to take this ellipse and turn it
     # into a curve patch on the surface of a sphere (the eye itself). If it's not a sphere, see your
@@ -347,7 +360,9 @@ def RANSAC3D(self, hsrac_en):
         # Black magic happens here, but after this we have our reprojected pupil/eye, and all we had
         # to do was sell our soul to satan and/or C++.
 
-        result_3d = self.detector_3d.update_and_detect(result_2d_final, self.current_image_gray)
+        result_3d = self.detector_3d.update_and_detect(
+            result_2d_final, self.current_image_gray
+        )
 
         # Now we have our pupil
         ellipse_3d = result_3d["ellipse"]
@@ -357,7 +372,7 @@ def RANSAC3D(self, hsrac_en):
         # Record our pupil center
         exm = ellipse_3d["center"][0]
         eym = ellipse_3d["center"][1]
-        #  print(result_2d["angle"])
+    #  print(result_2d["angle"])
         d = result_3d["diameter_3d"]
         self.cc_radius = int(float(self.lkg_projected_sphere["axes"][0]))
         self.xc = int(float(self.lkg_projected_sphere["center"][0]))
@@ -375,7 +390,9 @@ def RANSAC3D(self, hsrac_en):
             cy = self.rawy
         else:
             #  print(int(cx), int(clamp(cx + ransac_lower_x, 0, csx)), ransac_lower_x, csx, "y", int(cy), int(clamp(cy + ransac_lower_y, 0, csy)), ransac_lower_y, csy)
-            cx = int(clamp(cx + ransac_lower_x, 0, csx))  # dunno why this is being weird
+            cx = int(
+                clamp(cx + ransac_lower_x, 0, csx)
+            )  # dunno why this is being weird
             cy = int(clamp(cy + ransac_lower_y, 0, csy))
 
     # print(contours)
@@ -412,6 +429,7 @@ def RANSAC3D(self, hsrac_en):
                         for item in self.blink_list:
                             file.write(str(item) + "\n")
 
+
                 if self.eye_id in [EyeId.RIGHT]:
                     with open("RANSAC_BLINK_RIGHT.cfg", "w") as file:
                         for item in self.blink_list:
@@ -429,7 +447,9 @@ def RANSAC3D(self, hsrac_en):
                 blink = 0.0
 
     try:
-        cv2.drawContours(self.current_image_gray, contours, -1, (255, 0, 0), 1)  # TODO: fix visualizations with HSRAC
+        cv2.drawContours(
+            self.current_image_gray, contours, -1, (255, 0, 0), 1
+        )  # TODO: fix visualizations with HSRAC
         cv2.circle(self.current_image_gray, (int(cx), int(cy)), 2, (0, 0, 255), -1)
     except:
         pass
@@ -463,10 +483,10 @@ def RANSAC3D(self, hsrac_en):
 
         # draw line from center of eyeball to center of pupil
         cv2.line(
-            self.current_image_gray,
-            tuple(int(v) for v in self.lkg_projected_sphere["center"]),
-            tuple(int(v) for v in ellipse_3d["center"]),
-            (0, 255, 0),  # color (BGR): red
+        self.current_image_gray,
+        tuple(int(v) for v in self.lkg_projected_sphere["center"]),
+        tuple(int(v) for v in ellipse_3d["center"]),
+        (0, 255, 0),  # color (BGR): red
         )
 
     except:
